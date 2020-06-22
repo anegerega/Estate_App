69d3f08ca5ea2f418c83002533ac243c1175e416
anegerega <angelaegerega@yahoo.com>
2020-06-22 21:27:40 +0100

    Added Authentication DbContext Class

---
+++ src/EstateApp.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace EstateApp.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
